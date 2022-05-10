fa7371443b85408c955575c2c92264291fb0e8e5
David Alliger <alligerdavidj@gmail.com>
2022-05-10 14:26:26 -0500

    Got servo to fetch and disply responses from OpenAI.

--- src/components/servo/index.js
+++ src/components/servo/index.js
@@ -1,16 +1,71 @@
 import { useState, useEffect } from 'react';
 import './Servo.css'
 
+
 const Servo = () => {
     const [responses, setResponses] = useState([]);
+    const [query, setQuery] = useState({});
+    const [errors, setErrors] = useState([]);
+    const [prompt, setPrompt] = useState('');
+    const apiKey = process.env.REACT_APP_OPENAI_SECRET;
+
     useEffect(() => {
         if (localStorage.getItem("responses")) {
-            setResponses(localStorage.getItem("responses"));
+            setResponses(JSON.parse(localStorage.getItem("responses")));
         }
     }, []);
 
-    const handleSubmit = () => {
+    useEffect(() => {
+        console.log(responses);
+        if (responses.length) {
+            localStorage.setItem("responses", JSON.stringify(responses));
+        }
+    }, [responses]);
 
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        if (!query.prompt || !query.prompt.length) {
+            setErrors(['Please enter a prompt.'])
+        } else {
+            const response = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${apiKey}`
+                },
+                body: JSON.stringify(query)
+            });
+            console.log(response);
+            if (response.ok) {
+                const newResponse = await response.json();
+                console.log(newResponse);
+                const answer = [query.prompt, newResponse.choices[0].text];
+                console.log(answer);
+                setResponses([answer, ...responses]);
+                setPrompt('');
+            } else if (response.status < 500) {
+                const data = await response.json();
+                console.log(data);
+                if (data.error) {
+                    setErrors([data.error.message]);
+                }
+            } else {
+                setErrors(['An error occured. Please try again.'])
+            }
+        }
+    }
+
+    const handleChange = (e) => {
+        setQuery({
+            prompt: e.target.value,
+            temperature: 0.5,
+            max_tokens: 64,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0
+        });
+        setPrompt(e.target.value);
+        setErrors([]);
     }
 
     return (
@@ -38,10 +93,31 @@ const Servo = () => {
                 onSubmit={handleSubmit}
             >
                 <label id="label">
-                    <textarea id="textarea" />
+                    <textarea
+                        id="textarea"
+                        onChange={handleChange}
+                        value={prompt}
+                    />
                     <button id="button">Submit</button>
                 </label>
             </form>
+            <div id="errors">
+                {errors.map((error, index) => (
+                    <div className="error" key={`error-${index}`}>{error}</div>
+                ))}
+            </div>
+            <div id="responses">
+                {responses.map((response, index) => (
+                    <div className="response-box" key={`response-${index}`}>
+                        <div className="user-prompt">
+                            <span className="bold">Prompt:</span> {response[0]}
+                        </div>
+                        <div className="servo-says">
+                            <span className="bold">Servo Says:</span> {response[1]}
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
