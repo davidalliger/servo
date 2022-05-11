fccb94c496c8735a10ac8609302a36883509b023
David Alliger <alligerdavidj@gmail.com>
2022-05-11 12:34:04 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.static(__dirname + '/'));
 
 app.listen(process.env.PORT || 5000);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('/build'));
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+}
