4c5932942b17b1dbb499fd91d5793015178359e8
David Alliger <alligerdavidj@gmail.com>
2022-05-11 14:31:20 -0500

    Trying again.

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const path = require('path');
 
 const app = express();
 
-// if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname+ '/frontend/build/index.html'));
-    });
-// }
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-// app.use(express.static(__dirname + '/'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+ '/frontend/build/index.html'));
+});
+
 
 app.listen(process.env.PORT || 5000);
