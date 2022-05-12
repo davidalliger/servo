6184497f214e1645caeafde24a41cf1198ab348c
David Alliger <alligerdavidj@gmail.com>
2022-05-11 19:39:25 -0500

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-function Root() {
-  return (
-    <App />
-  )
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
